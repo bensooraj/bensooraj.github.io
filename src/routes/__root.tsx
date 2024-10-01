c54efbbc5959d42ba489477daedd77f13e43772f
bensooraj <bensooraj@users.noreply.github.com>
2024-10-02 00:33:44 +0530

    disable tanstack dev tools in production ref: https://tanstack.com/router/latest/docs/framework/react/devtools#only-importing-and-using-devtools-in-development

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react'
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Suspense } from 'react'
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+      // Lazy load in development
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      })),
+    )
 
 export const Route = createRootRoute({
   component: () => (
@@ -27,7 +40,9 @@ export const Route = createRootRoute({
         </div>
       </header>
       <Outlet />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </React.Fragment>
   ),
 })
